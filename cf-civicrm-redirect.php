dbc394bb1fa8c363c18a486fec3dff72168ebdaf
Aidan Saunders <aidan.saunders@squiffle.uk>
2021-04-20 15:26:21 +0100

    Add `{login_redirect_url}` Fix filter removal on redirect

--- cf-civicrm-redirect.php
+++ cf-civicrm-redirect.php
@@ -8,58 +8,77 @@
 */
 
 // Remove the default behaviour
-remove_filter('caldera_forms_submit_redirect_complete', array('Caldera_Forms', 'do_redirect'), 10);   
+// Don't know whether there is a better way...
+// Need to remove the default 'do_redirect' filter, and needs to be after CF is loaded.
+// We need the class instance to remove it.  Looks like get_instance() is how we should get hold of that.
+// But we can't use 'caldera_forms_core_init' since that is in _construct() so get_instance() loops
+// Might be easier if 'caldera_forms_core_init passed $this to the action ... but it doesn't.
+// So use 'plugins_loaded'
+add_action( 'plugins_loaded', function() {
+  $instance = Caldera_Forms::get_instance();
+  remove_filter('caldera_forms_submit_redirect_complete', array($instance, 'do_redirect'), 10);
+});
 
 // Add our own handling
-add_filter('caldera_forms_submit_redirect_complete', function($referrer, $form, $processid) 
-  {
-    if (isset($form['processors'])) {
-      foreach ($form['processors'] as $processor) {
-        if ($processor['type'] == 'form_redirect') {
-   
-          if (isset($processor['conditions']) && !empty($processor['conditions']['type'])) {
-            if (!Caldera_Forms::check_condition($processor['conditions'], $form)) {
-              continue;
-            }
-          }
-          if (!empty($processor['config']['url'])) {
- 
-             // set message
-             add_filter('caldera_forms_render_notices', array(
-               'Caldera_Forms',
-               'override_redirect_notice'
-             ), 10, 2);
-    
-             // Don't mess about with query args - we're redirecting to a civi page with arguments
-             // Can't call Caldera_Forms::do_magic_tags because it the encoded url from the '{get:redirect_to}'
-             // includes '..%...%..' which gets wrongly interpreted as a magic field.
-             // Instead, call Caldera_Forms_Magic_Doer::do_bracket_magic() directly, ... and fix $referer
-             // $redirect = Caldera_Forms::do_magic_tags($processor['config']['url']);
-             
-             // String received, but array expected for get:* tags
-             if (!is_array($referrer)) {
-               $referrer = parse_url($referrer);
-               if (!empty($referrer['query'])) {
-                 parse_str($referrer['query'], $referrer['query']);
-               }
-             }
+add_filter('caldera_forms_submit_redirect_complete', function($referrer, $form, $processid) {
+  if (isset($form['processors'])) {
+    foreach ($form['processors'] as $processor) {
+      if ($processor['type'] == 'form_redirect') {
 
-             $redirect = Caldera_Forms_Magic_Doer::do_bracket_magic($processor['config']['url'], $form, null, null, $referrer);
-             return $redirect;
+        if (isset($processor['conditions']) && !empty($processor['conditions']['type'])) {
+          if (!Caldera_Forms::check_condition($processor['conditions'], $form)) {
+            continue;
           }
+        }
+        if (!empty($processor['config']['url'])) {
+
+           // set message
+           add_filter('caldera_forms_render_notices', array(
+             'Caldera_Forms',
+             'override_redirect_notice'
+           ), 10, 2);
+
+           // Don't mess about with query args - we're redirecting to a civi page with arguments
+           // Can't call Caldera_Forms::do_magic_tags because it the encoded url from the '{get:redirect_to}'
+           // includes '..%...%..' which gets wrongly interpreted as a magic field.
+           // Instead, call Caldera_Forms_Magic_Doer::do_bracket_magic() directly, ... and fix $referer
+           // $redirect = Caldera_Forms::do_magic_tags($processor['config']['url']);
+
+           // String received, but array expected for get:* tags
+           if (!is_array($referrer)) {
+             $referrer = parse_url($referrer);
+             if (!empty($referrer['query'])) {
+               parse_str($referrer['query'], $referrer['query']);
+             }
+           }
+
+           $redirect = Caldera_Forms_Magic_Doer::do_bracket_magic($processor['config']['url'], $form, null, null, $referrer);
+           return $redirect;
         }
       }
     }
-    return $referrer;
-  }, 10, 4);
+  }
+  return $referrer;
+}, 10, 3);
 
-// Gross ugly hack... bypass %field% magic processing if value contains 'redirect_to' so is a URL
-add_filter('caldera_forms_pre_do_field_magic', function($default, $value, $matches, $entry_id, $form)
-  {
-    if (strpos($value, 'redirect_to') !== FALSE) {
-      return $value;
-    }
-    else {
-      return $default;
-    }
-  }, 10, 7);
+// Define our own magic tag of '{login_redirect_url}' using the redirect_to param
+add_filter('caldera_forms_do_magic_tag', function($value, $tag) {
+  if ($tag == '{login_redirect_url}') {
+    $redir = $_GET['redirect_to'] ?? '';
+    $redir = Caldera_Forms_Sanitize::sanitize($redir);
+    $value = wp_login_url($redir);
+  }
+  return $value;
+}, 10, 2);
+
+
+// Bypass %field% magic processing if value contains 'redirect_to' so is a URL
+// Needed for {login_redirect_url}
+add_filter('caldera_forms_pre_do_field_magic', function($default, $value, $matches, $entry_id, $form) {
+  if (strpos($value, 'redirect_to') !== FALSE) {
+    return $value;
+  }
+  else {
+    return $default;
+  }
+}, 10, 5);
--- readme.txt
+++ readme.txt
@@ -11,6 +11,8 @@ Helper plugin for Caldera Forms to make redirections from a CF form to a CiviCRM
 
 Caldera Forms includes a Redirect Processor that redirects the user to a specified URL on successful submission of the form.  In theory, the redirection URL can be passed to the form as a query parameter but bugs prevent that from working.
 
+Also provides '{login_redirect_url}' which generates the url for the login page include a redirect to the url provided as the 'redirect_to' argument.
+
 == Installation ==
 
 Upload the cf-civicrm-redirect folder to /wp-content/plugins/
@@ -19,8 +21,11 @@ No configuration is required.
 
 == Usage ==
 
-Configure your Caldera Form as usual.  Add the Redirect Processor and configure it to use eg '{get:redirect_to}'
+Configure your Caldera Form as usual.  Add the Redirect Processor and configure it to use '{get:redirect_to}'
 When you link to the Caldera Form, include the parameter 'redirect_to=' and the urlencoded CiviCRM.  Eg 'redirect_to=%2Fcivicrm%3Fciviwp%3DCiviCRM%26q%3Dcivicrm%252Fevent%252Fregister%26id%3D7%26reset%3D%1'
+
+The form can include a link to the login page using e.g.:
+'If you have a user account please <a href={login_redirect_url} login here </a>'
 
 == Example ==
 
@@ -32,4 +37,3 @@ Problems fixed by this plugin:
 1) Caldera_Forms_Magic_Doer::do_bracket_magic() is passed a $referrer as a string, but the processing for '{get:*}' tags expects an array
 2) Caldera_Forms::do_redirect() adds the calling query params to the redirection url.  Not sure what the use case is for that, but when redirecting to CiviCRM url's we don't want the original params, including this redirect
 3) Caldera_Forms::do_magic_tags() attempts to expand '{get.*}' magic and then expand field tags.  Since the encoded CiviCRM URL includes '...%...%...', the field expansion mangles the URL.
-
